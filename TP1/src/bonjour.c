beec86c1602990fb10366b70f899be250416737b
Mehdi <naouimehdi@hotmail.fr>
2025-11-21 09:56:57 +0100

    Update bonjour.c and cercle.c to improve greeting message and use M_PI for calculations; add settings.json for file associations

--- TP1/src/bonjour.c
+++ TP1/src/bonjour.c
@@ -1,7 +1,14 @@
+/*
+Mehdi NAOUI
+B3 + L CNAM
+21/11/2025
+ESEITT
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-printf("Bonjour l'ISEP");
+printf("Bonjour le Monde!");
 }
 
--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,21 @@
+/*
+Mehdi NAOUI
+B3 + L CNAM
+21/11/2025
+ESEITT
+*/
+
 #include <stdio.h>
 #include <string.h>
+//Import et définition de PI via Math.h
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 
 int main() {
 double pi = 3.1416;
 double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    printf("L'air du cercle = %f\n", M_PI*(r*r));
+   printf("Le périmètre du cercle = %f", M_PI*(r+r));
 }
 
