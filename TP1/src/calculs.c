5a8c08b9e72da6722d5438a374b720dc3db448a7
Mehdi <naouimehdi@hotmail.fr>
2025-11-22 00:15:13 +0100

    Refactor calculs.c to improve code structure and readability

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,39 @@
 
+/*
+Mehdi NAOUI
+B3 + L CNAM
+21/11/2025
+ESEITT
+*/
+#include <stdio.h>
+int num1 = 2 ;
+int num2 = 4 ;
+char op = '-';
+
+int main(int argc, char const *argv[])
+{
+    switch(op)
+{
+    case ('+'):
+    printf("Somme de %d + %d = %d",num1,num2,num1 + num2);
+    break;
+    case ('-'):
+    printf("Somme de %d - %d = %d",num1,num2,num1 - num2);
+    break;
+    case ('/'):
+    printf("Somme de %d / %d = %d",num1,num2,num1 / num2);
+    break;
+    case ('*'):
+    printf("Somme de %d * %d = %d",num1,num2,num1 * num2);
+    break;
+
+
+
+default:
+    break;
+}
+
+
+    return 0;
+}
+
